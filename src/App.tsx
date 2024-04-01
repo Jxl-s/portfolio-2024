d4ace53981f1f7424bba656b3b25759a583f40c3
Jxl-s <77560326+Jxl-s@users.noreply.github.com>
2024-04-01 12:23:29 -0400

    add text anim

--- src/App.tsx
+++ src/App.tsx
@@ -8,15 +8,13 @@ import {
     MapPinIcon,
     QuestionMarkCircleIcon,
 } from "@heroicons/react/24/outline";
-import { Canvas } from "@react-three/fiber";
-import HomeScene from "./3D/HomeScene";
 import i18n from "i18next";
 import { initReactI18next } from "react-i18next";
 
 import messagesEn from "./locales/en.json";
 import messagesFr from "./locales/fr.json";
 import Nav from "./components/Nav";
-import Button from "./components/Button";
+import Home from "./2D/Home";
 
 i18n.use(initReactI18next) // passes i18n down to react-i18next
     .init({
@@ -52,61 +50,9 @@ function App() {
             />
 
             <Nav />
-            <div className="bg-indigo-900/50 min-h-dvh p-4 lg:p-8 flex flex-col" id="home-div">
-                <main className="grid grid-cols-2 flex-grow items-center mt-[72px] lg:mt-0">
-                    <section className="text-center col-span-2 p-6 lg:col-span-1 items-center justify-center flex">
-                        <div className="max-w-xl">
-                            <h2 className="text-xl">HELLO</h2>
-                            <h1 className="text-6xl font-bold mt-2">
-                                I'm <span className="text-indigo-500">Jia</span>
-                            </h1>
-                            <h2 className="text-xl">
-                                a <span className="text-indigo-500">Computer Science Student</span>
-                            </h2>
-                            <div className="my-4" />
-                            <div className="flex items-center justify-center">
-                                <div className="h-2 bg-indigo-500 w-[90px]" />
-                            </div>
-                            <div className="my-4" />
-                            <p>
-                                Passionate in problem solving, creative full-stack development and
-                                software engineering.
-                            </p>
-                            <div className="my-4" />
-                            <div className="grid grid-cols-2 px-10 gap-4">
-                                <a href="https://github.com/Jxl-s/" target="_blank">
-                                    <Button color="black">
-                                        <span className="font-semibold">GitHub</span>
-                                    </Button>
-                                </a>
-                                <a href="https://www.linkedin.com/in/li-jiaxuan/" target="_blank">
-                                    <Button color="blue">
-                                        <span className="font-semibold">LinkedIn</span>
-                                    </Button>
-                                </a>
 
-                                <a className="col-span-2 font-semibold text-indigo-500">
-                                    Download my Resume (PDF)
-                                </a>
-                            </div>
-                        </div>
-                    </section>
-                    <section className="text-center col-span-2 p-6 lg:col-span-1">
-                        <div className="w-full h-[400px] bg-black">
-                            <Canvas className="w-full h-full rounded-lg border border-indigo-500/50 cursor-pointer">
-                                <HomeScene />
-                            </Canvas>
-                        </div>
-                        <p className="text-center mt-2">
-                            (Click to view the 3D version of my portfolio)
-                        </p>
-                    </section>
-                </main>
-                <div className="text-center w-full bg-red-500">
-                    Note: I am still developing this website. If you happened to find this by
-                    accident, it's not complete :D
-                </div>
-            </div>
+            {/* Add pages now */}
+            <Home />
 
             <div
                 className="bg-indigo-900/70 min-h-dvh p-4 lg:p-8 flex flex-col items-center"
