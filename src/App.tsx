73f8b5ca86545081dd39b8f2d4fcaf9cf3b1e274
Jxl-s <77560326+Jxl-s@users.noreply.github.com>
2024-04-03 13:13:30 -0400

    add suspense

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import { initReactI18next } from "react-i18next";
 import messagesEn from "./locales/en.json";
 import messagesFr from "./locales/fr.json";
 import Website2D from "./2D";
+import { Suspense } from "react";
 
 // Initialize i18n
 i18n.use(initReactI18next).init({
@@ -22,7 +23,9 @@ function App() {
     return (
         <>
             <div className="bg-black fixed top-0 bottom-0 w-full h-full -z-10" />
-            <Website2D />
+            <Suspense fallback={<p>please wait</p>}>
+                <Website2D />
+            </Suspense>
         </>
     );
 }
--- src/components/FadeIn.tsx
+++ src/components/FadeIn.tsx
@@ -1,5 +1,4 @@
 import { PropsWithChildren, useEffect, useState } from "react";
-import "./FadeIn.css"; // Import SCSS file for styling
 
 interface Props {
     delay: number;
@@ -14,7 +13,11 @@ const FadeInText = ({ delay, children }: PropsWithChildren<Props>) => {
         }, delay * 1000);
     });
 
-    return <div className={startFade ? 'fade-in' : 'opacity-0 translate-y-0'}>{children}</div>;
+    return (
+        <div className={startFade ? "fade-in" : "opacity-0 translate-y-0"}>
+            {children}
+        </div>
+    );
 };
 
 export default FadeInText;
