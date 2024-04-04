7d25519dd8f0985a04126afe6ef0818bafa27bb2
Jxl-s <77560326+Jxl-s@users.noreply.github.com>
2024-04-04 00:11:46 -0400

    add i18n to "open demo"

--- src/2D/Projects.tsx
+++ src/2D/Projects.tsx
@@ -262,7 +262,7 @@ interface ProjectCardProps {
 }
 
 function ProjectCard({ project }: ProjectCardProps) {
-    const { i18n } = useTranslation();
+    const { t, i18n } = useTranslation();
 
     const tags = i18n.language === "fr" ? project.tagsFr! : project.tags;
     const desc = i18n.language === "fr" ? project.descFr! : project.desc;
@@ -294,7 +294,7 @@ function ProjectCard({ project }: ProjectCardProps) {
                 <img src={project.image} className="w-full" loading="lazy" />
                 {project.demo && (
                     <div className="absolute top-0 left-0 w-full h-full opacity-0 hover:opacity-80 duration-300 bg-black flex items-center justify-center font-semibold text-xl">
-                        Open Demo
+                        {t("Open Demo")}
                     </div>
                 )}
             </a>
