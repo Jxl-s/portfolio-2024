893db17da13912cc3184411887e73d4d982d5948
Jxl-s <77560326+Jxl-s@users.noreply.github.com>
2024-04-03 21:55:56 -0400

    use style instead of keyframes for fadein

--- src/components/FadeIn.tsx
+++ src/components/FadeIn.tsx
@@ -1,20 +1,29 @@
-import { PropsWithChildren, useEffect, useState } from "react";
+import { PropsWithChildren, useEffect, useRef } from "react";
 
 interface Props {
     delay: number;
 }
 
 const FadeInText = ({ delay, children }: PropsWithChildren<Props>) => {
-    const [startFade, setStartFade] = useState(false);
-
+    const divRef = useRef<HTMLDivElement>(null);
     useEffect(() => {
         setTimeout(() => {
-            setStartFade(true);
+            if (!divRef.current) return;
+
+            divRef.current.style.opacity = "100%";
+            divRef.current.style.transform = "translateY(0)";
         }, delay * 1000);
     });
 
     return (
-        <div className={startFade ? "fade-in" : "opacity-0 translate-y-0"}>
+        <div
+            ref={divRef}
+            style={{
+                opacity: 0,
+                transform: "translateY(20px)",
+                transitionDuration: "1s",
+            }}
+        >
             {children}
         </div>
     );
