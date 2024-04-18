831f3d18dbf831f8c366cdd3480030a0897e14c6
Jxl-s <77560326+Jxl-s@users.noreply.github.com>
2024-04-18 00:47:00 -0400

    change board colors

--- src/3D/models/Scene.tsx
+++ src/3D/models/Scene.tsx
@@ -178,35 +178,37 @@ export default function Scene(props: JSX.IntrinsicElements["group"]) {
                     occlude={"blending"}
                 >
                     <div
-                        className="text-white text-3xl bg-indigo-500/80 rounded-lg text-center p-4 select-none"
+                        className="text-white text-3xl bg-blue-700/80 rounded-lg text-center p-2 select-none"
                         style={{
                             width: "378px",
                             height: "540px",
                         }}
                     >
-                        <b>🍎 Jia's Market 🥟</b>
-                        <span className="text-2xl block">
-                            We are <b className="text-green-500">OPEN</b>
-                        </span>
-                        <hr className="border-2 my-4" />
-                        <ul className="text-4xl mt-10 flex items-center justify-center flex-col gap-8 font-mono flex-grow">
-                            <li className=" hover:text-blue-300 duration-300 cursor-pointer">
-                                ABOUT ME
-                            </li>
-                            <li className=" hover:text-blue-300 duration-300 cursor-pointer">
-                                PROJECTS
-                            </li>
-                            <li className=" hover:text-blue-300 duration-300 cursor-pointer">
-                                JOURNEY
-                            </li>
-                            <li className=" hover:text-blue-300 duration-300 cursor-pointer">
-                                CONTACT
-                            </li>
-                        </ul>
-                        <hr className="border-2 mt-12 mb-8" />
-                        <p className="text-sm">
-                            © 2024 Jia Xuan Li. All rights reserved.
-                        </p>
+                        <div className="border-4 rounded-lg border-blue-300 w-full h-full p-4">
+                            <b>🍎 Jia's Market 🥟</b>
+                            <span className="text-2xl block">
+                                We are <b className="text-green-500">OPEN</b>
+                            </span>
+                            <hr className="border-2 my-4" />
+                            <ul className="text-4xl mt-10 flex items-center justify-center flex-col gap-8 font-mono flex-grow">
+                                <li className=" hover:text-blue-300 duration-300 cursor-pointer">
+                                    <b>ABOUT ME</b>
+                                </li>
+                                <li className=" hover:text-blue-300 duration-300 cursor-pointer">
+                                    <b>PROJECTS</b>
+                                </li>
+                                <li className=" hover:text-blue-300 duration-300 cursor-pointer">
+                                    <b>JOURNEY</b>
+                                </li>
+                                <li className=" hover:text-blue-300 duration-300 cursor-pointer">
+                                    <b>CONTACT</b>
+                                </li>
+                            </ul>
+                            <hr className="border-2 mt-12 mb-8" />
+                            <p className="text-sm">
+                                © 2024 Jia Xuan Li. All rights reserved.
+                            </p>
+                        </div>
                     </div>
                 </Html>
             </mesh>
