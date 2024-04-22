8506eacfd56f31acdd983f8a05bf929fce909fbb
Jxl-s <77560326+Jxl-s@users.noreply.github.com>
2024-04-22 01:22:20 -0400

    remove move sound

--- src/3D/Experience/index.tsx
+++ src/3D/Experience/index.tsx
@@ -15,7 +15,7 @@ import { button, useControls } from "leva";
 import useCameraStore, { positions } from "../Stores/useCameraStore";
 import { useThree } from "@react-three/fiber";
 import Decoration from "./Decoration";
-import { playSound } from "../../util/sound";
+// import { playSound } from "../../util/sound";
 
 export default function Experience() {
     const { controls, camera } = useThree();
@@ -101,7 +101,7 @@ export default function Experience() {
         (controls as TOrbitControls).enableRotate = focus === "home";
 
         // Move the camera and target
-        playSound("move.wav");
+        // playSound("move.wav");
         gsap.to(camera.position, {
             x: positions[focus].position[0],
             y: positions[focus].position[1],
