5be454528d493615814c5fa62c41c4f3a6e3408f
Jxl-s <77560326+Jxl-s@users.noreply.github.com>
2024-06-02 17:20:30 -0400

    vending machine optimizations

--- src/3D/Objects/ScreenVending.tsx
+++ src/3D/Objects/ScreenVending.tsx
@@ -1,21 +1,70 @@
-import { Html } from "@react-three/drei";
+import { Html, useTexture } from "@react-three/drei";
 import Projects from "../Interfaces/Projects";
 // import { CameraFocus } from "../Data/cameraPositions";
 // import Help3D from "../Components/Help3D";
 // import { useTranslation } from "react-i18next";
+import { registerPulseMaterial } from "../Materials";
+import { CameraFocus } from "../Data/cameraPositions";
+import { ThreeEvent, useFrame } from "@react-three/fiber";
+import * as THREE from "three";
+import useExperienceStore from "../Stores/useExperienceStore";
+import PulseMaterial from "../Materials/PulseMaterial";
 
 export default function ScreenVending(props: JSX.IntrinsicElements["mesh"]) {
     // const { t } = useTranslation();
+    const vendingTexture = useTexture("/textures/screens/vending.png");
+    vendingTexture.flipY = false;
+
+    const vendingMaterial = new PulseMaterial({
+        // @ts-expect-error Ignore this
+        uTexture: vendingTexture,
+        uTime: 0,
+    });
+
+    vendingMaterial.side = THREE.BackSide;
+    registerPulseMaterial(vendingMaterial);
+
+    useFrame((_, delta) => {
+        if (vendingMaterial.uniforms.uPause.value) return;
+        vendingMaterial.uniforms.uTime.value += delta;
+    });
+
+    const handleMeshEnter = () => {
+        vendingMaterial.uniforms.uPause.value = true;
+        document.body.style.cursor = "pointer";
+    };
+
+    const handleMeshLeave = () => {
+        vendingMaterial.uniforms.uPause.value = false;
+        document.body.style.cursor = "auto";
+    };
+
+    const [cameraFocus, setCameraFocus] = useExperienceStore((state) => [
+        state.cameraFocus,
+        state.setCameraFocus,
+    ]);
+
+    const handleMeshClick = (e: ThreeEvent<MouseEvent>) => {
+        e.stopPropagation();
+        setCameraFocus(CameraFocus.Projects);
+        document.body.style.cursor = "auto";
+    };
 
     return (
         <mesh {...props}>
-            {/* <Help3D
-                position={[0, 0.65, 0]}
-                focus={CameraFocus.Projects}
-                // className="bg-blue-700/50 hover:bg-blue-700/100"
-            >
-                {t("projects")}
-            </Help3D> */}
+            {cameraFocus !== CameraFocus.Projects && (
+                <mesh
+                    scale={[0.63, 0.86, 1]}
+                    position={[0.002, 0, 0]}
+                    rotation={[0, -Math.PI * 0.5, Math.PI]}
+                    onPointerEnter={handleMeshEnter}
+                    onPointerLeave={handleMeshLeave}
+                    onClick={handleMeshClick}
+                    material={vendingMaterial}
+                >
+                    <planeGeometry args={[1, 1]} />
+                </mesh>
+            )}
             <Html
                 center
                 transform
@@ -28,6 +77,8 @@ export default function ScreenVending(props: JSX.IntrinsicElements["mesh"]) {
                 style={{
                     width: "504px",
                     height: "690px",
+                    display:
+                        cameraFocus === CameraFocus.Projects ? "block" : "none",
                 }}
             >
                 <Projects />
