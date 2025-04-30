837eef8ae99b3480a4a3967f4347db13c93f3d4f
Jia <77560326+Jxl-s@users.noreply.github.com>
2025-04-30 18:15:02 -0400

    fix smoke

--- src/3D/Experience/DynamicDecorations.tsx
+++ src/3D/Experience/DynamicDecorations.tsx
@@ -7,69 +7,71 @@ import { getAsset } from "../Stores/useLoaderStore";
 const floaterGeometry = new THREE.IcosahedronGeometry(1, 1);
 
 export default function Decoration() {
-    // Little boxes
-    const boxRef = useRef<THREE.Mesh>(null);
-    const boxRef2 = useRef<THREE.Mesh>(null);
+	// Little boxes
+	const boxRef = useRef<THREE.Mesh>(null);
+	const boxRef2 = useRef<THREE.Mesh>(null);
 
-    // Coffee
-    const coffeeMaterial = useMemo(
-        () =>
-            new CoffeeMaterial({
-                side: THREE.DoubleSide,
-                transparent: true,
-                depthWrite: false,
-                uniforms: {
-                    uTime: { value: 0 },
-                    uPerlinTexture: { value: getAsset("perlin") },
-                },
-            }),
-        []
-    );
+	// Coffee
+	const coffeeMaterial = useMemo(() => {
+		const perlinAsset = getAsset<THREE.Texture>("perlin");
+		perlinAsset.wrapS = THREE.RepeatWrapping;
+		perlinAsset.wrapT = THREE.RepeatWrapping;
 
-    useFrame(({ clock }) => {
-        if (!boxRef.current) return;
-        if (!boxRef2.current) return;
+		return new CoffeeMaterial({
+			side: THREE.DoubleSide,
+			transparent: true,
+			depthWrite: false,
+			uniforms: {
+				uTime: { value: 0 },
+				uPerlinTexture: { value: perlinAsset },
+			},
+		});
+	}, []);
 
-        boxRef.current.position.y =
-            Math.abs(Math.sin(clock.getElapsedTime())) * 0.1 - 1.31;
-        boxRef.current.rotation.x = Math.sin(clock.getElapsedTime() * 2) * 0.5;
-        boxRef.current.rotation.y = Math.sin(clock.getElapsedTime() * 2) * 0.5;
+	useFrame(({ clock }) => {
+		if (!boxRef.current) return;
+		if (!boxRef2.current) return;
 
-        boxRef2.current.position.y =
-            Math.abs(Math.sin(clock.getElapsedTime())) * 0.1 - 1;
-        boxRef2.current.rotation.y = Math.sin(clock.getElapsedTime() * 2) * 0.5;
-        boxRef2.current.rotation.z = Math.sin(clock.getElapsedTime() * 2) * 0.5;
+		boxRef.current.position.y =
+			Math.abs(Math.sin(clock.getElapsedTime())) * 0.1 - 1.31;
+		boxRef.current.rotation.x = Math.sin(clock.getElapsedTime() * 2) * 0.5;
+		boxRef.current.rotation.y = Math.sin(clock.getElapsedTime() * 2) * 0.5;
 
-        coffeeMaterial.uniforms.uTime.value = clock.getElapsedTime();
-    });
+		boxRef2.current.position.y =
+			Math.abs(Math.sin(clock.getElapsedTime())) * 0.1 - 1;
+		boxRef2.current.rotation.y = Math.sin(clock.getElapsedTime() * 2) * 0.5;
+		boxRef2.current.rotation.z = Math.sin(clock.getElapsedTime() * 2) * 0.5;
 
-    return (
-        <>
-            {/* Balls floating on top of barrels */}
-            <mesh
-                scale={[0.1, 0.1, 0.1]}
-                position={[2.09, -1.3, 1.05]}
-                ref={boxRef}
-                geometry={floaterGeometry}
-            >
-                <meshStandardMaterial color="#fca453" flatShading={true} />
-            </mesh>
-            <mesh
-                scale={[0.15, 0.15, 0.15]}
-                position={[-1.6, -1, 1.2]}
-                ref={boxRef2}
-                geometry={floaterGeometry}
-            >
-                <meshStandardMaterial color="#888888" flatShading={true} />
-            </mesh>
+		coffeeMaterial.uniforms.uTime.value = clock.getElapsedTime();
+	});
 
-            <mesh
-                position={[-1.05, -0.4, 0.1]}
-                material={coffeeMaterial}
-                rotation-y={Math.PI * 0.5}
-            >
-                <planeGeometry args={[1.7, 0.7, 16, 64]} />
-            </mesh>
-        </>
-    );
+	return (
+		<>
+			{/* Balls floating on top of barrels */}
+			<mesh
+				scale={[0.1, 0.1, 0.1]}
+				position={[2.09, -1.3, 1.05]}
+				ref={boxRef}
+				geometry={floaterGeometry}
+			>
+				<meshStandardMaterial color="#fca453" flatShading={true} />
+			</mesh>
+			<mesh
+				scale={[0.15, 0.15, 0.15]}
+				position={[-1.6, -1, 1.2]}
+				ref={boxRef2}
+				geometry={floaterGeometry}
+			>
+				<meshStandardMaterial color="#888888" flatShading={true} />
+			</mesh>
+
+			<mesh
+				position={[-1.05, -0.4, 0.1]}
+				material={coffeeMaterial}
+				rotation-y={Math.PI * 0.5}
+			>
+				<planeGeometry args={[1.7, 0.7, 16, 64]} />
+			</mesh>
+		</>
+	);
 }
